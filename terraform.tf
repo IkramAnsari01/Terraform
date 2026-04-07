efd41dced1203dbf808bee4cf18b0c5061c41762
IkramAnsari01 <ikramansari21112001@gmail.com>
2026-04-07 13:27:25 +0530

    Update

--- terraform.tf
+++ terraform.tf
@@ -6,13 +6,13 @@ terraform {
     }
   }
 
-  backend "s3" {
-  bucket       = "common-terraform-bucket"
-  key          = "terraform.tfstate"
-  region       = "ap-south-1"
-  encrypt      = true
-  use_lockfile = true
-}
+#   backend "s3" {
+#   bucket       = "common-terraform-bucket"
+#   key          = "terraform.tfstate"
+#   region       = "ap-south-1"
+#   encrypt      = true
+#   use_lockfile = true
+# }
   ##  when uncommenting backend, in terminal write: terraform init -migrate-state to migrate the state file to s3 bucket  
   ##  then do terraform apply
 }
